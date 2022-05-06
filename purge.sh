03577579f04716e77bd223efa10f41c3c576fedc
Marc <xerokill@gmail.com>
2022-05-06 12:28:06 +0800

    added purge script

---
+++ purge.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 </path/to/resource>"
+    exit 1
+fi
+
+printf "[PURGING $1 ...]\n\n"
+docker exec demo_cdn curl -k -I -H 'Nuke-Cache: true' https://localhost$1
