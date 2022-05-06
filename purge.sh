4d20bac2b114e11de7d3f227aeb5dceca9b0cdd0
Marc <xerokill@gmail.com>
2022-05-06 18:31:01 +0800

    removed argument check

--- purge.sh
+++ purge.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 </path/to/resource>"
-    exit 1
-fi
-
 printf "[PURGING ENTIRE CACHE ...]\n\n"
 docker exec -it demo_cdn /bin/ash -c "rm -rf /var/cache/nginx/proxy_cache/*"
